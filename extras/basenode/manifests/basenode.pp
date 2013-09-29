748538bd1eea05018a35851f47a9dec97514452d
Scott O'Neil <scott@cpanel.net>
2013-09-29 17:37:34 -0500

    Adding presentation supporting material

---
+++ extras/basenode/manifests/basenode.pp
@@ -0,0 +1,35 @@
+class basenode {
+#    file { '/etc/motd':
+#        source => 'puppet:///modules/basenode/motd'
+#    }
+#    file { '/etc/ssh/sshd_config':
+#        source => 'puppet:///modules/basenode/sshd_config',
+#        notify => Service['sshd']
+#    }
+#    service { 'sshd':
+#    }
+#    cpanel::easyapache { 'puppet':
+#        source => 'puppet:///modules/basenode/easyapache.yaml',
+#        email  => 'some@example.com'
+#    }
+#    cpanel::tweaksetting { 'puppet':
+#        options => { 
+#                     'ftpserver'  => 'pure-ftpd',
+#                     'mailserver' => 'dovecot'
+#                   },
+#	email   => 'some@example.com'
+#    }
+#    cpanel::baseconfig { 'puppet':
+#        host         => 'test.cpanel.net',
+#        ns           => 'test-a.cpanel.net',
+#        ns2          => 'test-b.cpanel.net',
+#        contactemail => 'some@example.com'
+#    }
+#    cpanel::cpanelaccount{ 'puppet':
+#	email       => 'some@example.com',
+#       domain      => 'thisisadomain.com',
+#       user        => 'someuser',
+#	ensure      => 'present'
+#    }
+}
+
---
+++ extras/manifests/site.pp
@@ -0,0 +1,5 @@
+import "cpanel"
+import "basenode"
+node default {
+    include basenode
+}
